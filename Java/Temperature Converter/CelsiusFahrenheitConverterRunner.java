411bf4347919369e069399645abe117fe5e412ce
adammagnotte <adammagnotte@gmail.com>
2024-05-08 20:48:41 -0400

    Add files via upload

---
+++ Java/Temperature Converter/CelsiusFahrenheitConverterRunner.java
@@ -0,0 +1,14 @@
+package ex26_13FarenheitConverterTwo;
+
+import chapter26testing.*;
+import javax.swing.*;
+
+public class CelsiusFahrenheitConverterRunner {
+    public static void main(String[] args) {
+    CelsiusFahrenheitConverter celsiusFahrenheitConverter = new CelsiusFahrenheitConverter();
+    celsiusFahrenheitConverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    celsiusFahrenheitConverter.setSize(400, 250);
+    celsiusFahrenheitConverter.setVisible(true);
+    celsiusFahrenheitConverter.setLocationRelativeTo(null);
+    }
+}
